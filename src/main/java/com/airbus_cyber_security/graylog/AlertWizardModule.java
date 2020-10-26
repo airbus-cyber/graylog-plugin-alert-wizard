200b958fde9fee762c872762bef04e5846dde989
tomasnk <thomas.oudin@gmail.com>
2020-10-26 19:01:05 +0100

    add Copyright header

--- src/main/java/com/airbus_cyber_security/graylog/AlertWizardModule.java
+++ src/main/java/com/airbus_cyber_security/graylog/AlertWizardModule.java
@@ -1,3 +1,23 @@
+/*
+ * graylog-plugin-alert-wizard Source Code
+ * Copyright (C) 2018-2020 - Airbus CyberSecurity (SAS) - All rights reserved
+ *
+ * This file is part of the graylog-plugin-alert-wizard GPL Source Code.
+ *
+ * graylog-plugin-alert-wizard Source Code is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this code.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.airbus_cyber_security.graylog;
 
 import com.airbus_cyber_security.graylog.alert.AlertRuleService;
