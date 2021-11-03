8caaaa69e80622cab4da2dcb5e2c5ce78e1edbb9
c8y3 <25362953+c8y3@users.noreply.github.com>
2021-11-03 13:14:41 +0100

    Added test on GET of alerts/data

--- validation/test.py
+++ validation/test.py
@@ -29,11 +29,16 @@ class Test(TestCase):
 
     def _get(self, path):
         url = self._build_url(path)
+        print(url)
         return requests.get(url, auth=_AUTH, headers=_HEADERS)
 
     def _put(self, path, payload):
         url = self._build_url(path)
         requests.put(url, json=payload, auth=_AUTH, headers=_HEADERS)
+
+    def test_get_alerts_should_be_found(self):
+        response = self._get('alerts/data')
+        self.assertEqual(200, response.status_code)
 
     def test_put_config_with_time_default_value_should_modify_time_default_value(self):
         payload = {
@@ -50,7 +55,7 @@ class Test(TestCase):
         self.assertEqual(1441, body["default_values"]["time"])
 
     # TODO
-    #def test_default_time_range_in_configuration_should_propagate_into_notification_time_range__issue47(self):
-    #   requests.put('http://127.0.0.1:9000/api/plugins/com.airbus_cyber_security.graylog/config', json=_PAYLOAD, auth=('admin', 'admin'), headers=_HEADERS)
+#    def test_default_time_range_in_configuration_should_propagate_into_notification_time_range__issue47(self):
+#       requests.put('http://127.0.0.1:9000/api/plugins/com.airbus_cyber_security.graylog/config', json=_PAYLOAD, auth=('admin', 'admin'), headers=_HEADERS)
         # create an alert rule
         # check the associated notification aggregation time range should be 1441 (the value set in the put request)
