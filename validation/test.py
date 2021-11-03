7c6425a43e9de0e9f56fa60b93cf741ff5192560
c8y3 <25362953+c8y3@users.noreply.github.com>
2021-11-03 11:24:31 +0100

    Factored end-to-end tests somewhat

--- validation/test.py
+++ validation/test.py
@@ -7,8 +7,11 @@
 from unittest import TestCase
 from graylog_server import GraylogServer
 import requests
+from urllib import parse
 
+_AUTH = ('admin', 'admin')
 _HEADERS = {"X-Requested-By": "test-program"}
+_URL_PREFIX = 'http://127.0.0.1:9000/api/plugins/com.airbys_security.graylog'
 
 class Test(TestCase):
 
@@ -21,6 +24,17 @@ class Test(TestCase):
     def tearDown(self) -> None:
         self._graylog.stop()
 
+    def _build_url(self, path):
+        return parse.urljoin('http://127.0.0.1:9000/api/plugins/com.airbus_cyber_security.graylog/', path)
+
+    def _get(self, path):
+        url = self._build_url(path)
+        return requests.get(url, auth=_AUTH, headers=_HEADERS)
+
+    def _put(self, path, payload):
+        url = self._build_url(path)
+        requests.put(url, json=payload, auth=_AUTH, headers=_HEADERS)
+
     def test_put_config_with_time_default_value_should_modify_time_default_value(self):
         payload = {
             "default_values": {
@@ -30,9 +44,8 @@ class Test(TestCase):
             },
             "field_order": []
         }
-        response = requests.put('http://127.0.0.1:9000/api/plugins/com.airbus_cyber_security.graylog/config', json=payload,
-                                auth=('admin', 'admin'), headers=_HEADERS)
-        response = requests.get('http://127.0.0.1:9000/api/plugins/com.airbus_cyber_security.graylog/config', auth=('admin', 'admin'), headers=_HEADERS)
+        self._put('config', payload)
+        response = self._get('config')
         body = response.json()
         self.assertEqual(1441, body["default_values"]["time"])
 
