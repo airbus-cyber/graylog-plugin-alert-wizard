a9ddd27acdeaa570013d27ae6d3a25275a9f3dbb
c8y3 <25362953+c8y3@users.noreply.github.com>
2022-05-18 10:08:18 +0200

    Flushing print in tests

--- validation/graylog_rest_api.py
+++ validation/graylog_rest_api.py
@@ -9,25 +9,28 @@ _HEADERS = {'X-Requested-By': 'test-program'}
 
 class GraylogRestApi:
 
+    def _print(self, message):
+        print(message, flush=True)
+
     def _build_url(self, path):
         return parse.urljoin('http://127.0.0.1:9000/api/', path)
 
     def get(self, path):
         url = self._build_url(path)
         response = requests.get(url, auth=_AUTH, headers=_HEADERS)
-        print('GET {} => {}'.format(url, response.status_code))
+        self._print('GET {} => {}'.format(url, response.status_code))
         return response
 
     def put(self, path, payload):
         url = self._build_url(path)
         response = requests.put(url, json=payload, auth=_AUTH, headers=_HEADERS)
-        print('PUT {} {} => {}'.format(url, payload, response.status_code))
+        self._print('PUT {} {} => {}'.format(url, payload, response.status_code))
         return response
 
     def post(self, path, payload=None):
         url = self._build_url(path)
         response = requests.post(url, json=payload, auth=_AUTH, headers=_HEADERS)
-        print('POST {} {} => {}'.format(url, payload, response.status_code))
+        self._print('POST {} {} => {}'.format(url, payload, response.status_code))
         return response
 
     def wait_until_graylog_has_started(self):
@@ -36,7 +39,7 @@ class GraylogRestApi:
         This might have to change in the future, if graylog changes its ways...
         :return:
         """
-        print('Waiting for graylog to start...')
+        self._print('Waiting for graylog to start...')
 
         # TODO move as a method in _graylog_rest_api
         #only for 60s maximum
