8a03391c96c769761075702e0203e1fee60f99fb
c8y3 <25362953+c8y3@users.noreply.github.com>
2022-09-06 15:44:35 +0200

    Removed unused import

--- src/web/wizard/components/ruletype/CorrelationCondition.jsx
+++ src/web/wizard/components/ruletype/CorrelationCondition.jsx
@@ -23,7 +23,6 @@ import createReactClass from 'create-react-class';
 import ObjectUtils from 'util/ObjectUtils';
 import { Button } from 'react-bootstrap';
 import { FormattedMessage } from 'react-intl';
-import TitleSeverity from 'wizard/components/TitleSeverity';
 import FieldsCondition from 'wizard/components/conditions/FieldsCondition';
 import NumberCondition from 'wizard/components/conditions/NumberCondition';
 import TimeRangeCondition from 'wizard/components/conditions/TimeRangeCondition';
