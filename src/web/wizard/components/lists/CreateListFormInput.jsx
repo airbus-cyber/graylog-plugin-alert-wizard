73caff1dc6ff0f470eab6e02fe4d327baaf6a4b1
c8y3 <25362953+c8y3@users.noreply.github.com>
2022-09-28 13:05:15 +0200

    Avoid Reflux mixin in CreateListFormInput

--- src/web/wizard/components/lists/CreateListFormInput.jsx
+++ src/web/wizard/components/lists/CreateListFormInput.jsx
@@ -33,12 +33,10 @@ const INIT_LIST = {
     title: '',
     description: '',
     lists: '',
-    };
+};
 
 const CreateListFormInput = createReactClass({
     displayName: 'CreateListFormInput',
-
-    mixins: [Reflux.connect(AlertListStore)],
 
     propTypes: {
         list: PropTypes.object,
@@ -77,7 +75,7 @@ const CreateListFormInput = createReactClass({
     },
 
     _save() {
-        AlertListActions.create.triggerPromise(this.state.list).then((response) => {
+        AlertListActions.create(this.state.list).then((response) => {
             if (response === true) {
                 this.setState({list: list});
             }
@@ -86,7 +84,7 @@ const CreateListFormInput = createReactClass({
     },
 
     _update() {
-        AlertListActions.update.triggerPromise(this.props.list.title, this.state.list).then((response) => {
+        AlertListActions.update(this.props.list.title, this.state.list).then((response) => {
             if (response === true) {
                 this.setState({list: list});
             }
@@ -176,7 +174,7 @@ const CreateListFormInput = createReactClass({
                     <Col md={5}>
                     <Input style={{minWidth: 600}} ref="lists" id="lists" name="lists" type="textarea" rows="10"
                            label={<FormattedMessage id ="wizard.fieldListwithexemple" defaultMessage="List (example : 172.10.0.1; 192.168.1.4; ...)" />}
-                           onChange={this._onUpdate('lists')} defaultValue = {this.state.list.lists}/>
+                           onChange={this._onUpdate('lists')} defaultValue={this.state.list.lists} />
                         {actions}
                     </Col>
                 </Row>
