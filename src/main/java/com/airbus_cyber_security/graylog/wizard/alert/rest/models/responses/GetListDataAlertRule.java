0127c673bd1b05c8a3502909417c476e587f509e
c8y3 <25362953+c8y3@users.noreply.github.com>
2023-02-03 08:12:56 +0100

    Added TODO

--- src/main/java/com/airbus_cyber_security/graylog/wizard/alert/rest/models/responses/GetListDataAlertRule.java
+++ src/main/java/com/airbus_cyber_security/graylog/wizard/alert/rest/models/responses/GetListDataAlertRule.java
@@ -24,6 +24,7 @@ import com.google.auto.value.AutoValue;
 
 import java.util.List;
 
+// TODO maybe it is possible to remove this class (and simply use of List<GetDataAlertRule>)...
 @AutoValue
 @JsonAutoDetect
 public abstract class GetListDataAlertRule {
