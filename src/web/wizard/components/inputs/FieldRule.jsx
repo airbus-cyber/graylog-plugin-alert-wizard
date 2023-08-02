460442dc9a0479e0e6515b88e15d4295c7dbb495
c8y3 <25362953+c8y3@users.noreply.github.com>
2023-08-02 13:27:55 +0200

    Changed form into a div, since a form can't be the descendant of another form

--- src/web/wizard/components/inputs/FieldRule.jsx
+++ src/web/wizard/components/inputs/FieldRule.jsx
@@ -15,97 +15,126 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 
-
+// TODO remove this import?
 import PropTypes from 'prop-types';
+// TODO remove this import?
 import React from 'react';
-import { injectIntl, FormattedMessage } from 'react-intl';
-import Reflux from 'reflux';
-import createReactClass from 'create-react-class';
+import { useEffect, useState } from 'react';
+import { useIntl, FormattedMessage } from 'react-intl';
 import { Input } from 'components/bootstrap';
 import { Select, Spinner, TypeAheadFieldInput } from 'components/common';
 import ObjectUtils from 'util/ObjectUtils';
 import AlertListActions from 'wizard/actions/AlertListActions';
-import AlertListStore from 'wizard/stores/AlertListStore';
 import IconRemove from 'wizard/components/icons/Remove';
 import FormsUtils from 'util/FormsUtils';
-import { CurrentUserStore } from 'stores/users/CurrentUserStore';
 
 
-// TODO rewrite this as a functional component
-const FieldRule = createReactClass({
-    displayName: 'FieldRule',
+// TODO transform this into a constant? _AVAILABLE_RULE_TYPE
+const _availableRuleType = () => {
+    return [
+        {value: "1", label: <FormattedMessage id="wizard.matchesExactly" defaultMessage="matches exactly"/>},
+        {
+            value: "-1",
+            label: <FormattedMessage id="wizard.notMatchesExactly" defaultMessage="does not match exactly"/>
+        },
+        {
+            value: "2",
+            label: <FormattedMessage id="wizard.matchesRegularExpression"
+                                     defaultMessage="matches regular expression"/>
+        },
+        {
+            value: "-2",
+            label: <FormattedMessage id="wizard.notMatchRegularExpression"
+                                     defaultMessage="does not match regular expression"/>
+        },
+        {value: "3", label: <FormattedMessage id="wizard.greaterThan" defaultMessage="is greater than"/>},
+        {value: "-3", label: <FormattedMessage id="wizard.notGreaterThan" defaultMessage="is not greater than"/>},
+        {value: "4", label: <FormattedMessage id="wizard.smallerThan" defaultMessage="is smaller than"/>},
+        {value: "-4", label: <FormattedMessage id="wizard.notSmallerThan" defaultMessage="is not smaller than"/>},
+        {value: "5", label: <FormattedMessage id="wizard.present" defaultMessage="is present"/>},
+        {value: "-5", label: <FormattedMessage id="wizard.notPresent" defaultMessage="is not present"/>},
+        {value: "6", label: <FormattedMessage id="wizard.contains" defaultMessage="contains"/>},
+        {value: "-6", label: <FormattedMessage id="wizard.notContain" defaultMessage="does not contain"/>},
+        {value: "7", label: <FormattedMessage id="wizard.listpresent" defaultMessage="is present in list"/>},
+        {
+            value: "-7",
+            label: <FormattedMessage id="wizard.listnotpresent" defaultMessage="is not present in list"/>
+        },
+    ];
+};
 
-    mixins: [Reflux.connect(CurrentUserStore), Reflux.connect(AlertListStore)],
+const FieldRule = ({matchData, rule, onUpdate, onDelete, index}) => {
+    // TODO why the ObjectUtils.clone here?
+    // TODO split this state in several states with only one value
+    const [state, setState] = useState({
+        rule: ObjectUtils.clone(rule),
+        isModified: false,
+        isValid: false,
+        hover: false,
+        lists: null,
+        messages: messages
+    });
 
-    propTypes: {
-        rule: PropTypes.object.isRequired,
-        onUpdate: PropTypes.func.isRequired,
-        onDelete: PropTypes.func.isRequired,
-        matchData: PropTypes.array
-    },
-
-    getInitialState() {
-        const { intl } = this.props;
-        const messages = {
-            delete: intl.formatMessage({id: "wizard.delete", defaultMessage: "Delete"}),
-            select: intl.formatMessage({id: "wizard.select", defaultMessage: "Select..."})
-        };
-        return {
-            rule: ObjectUtils.clone(this.props.rule),
-            isModified: false,
-            isValid: false,
-            hover: false,
-            lists: null,
-            messages: messages
-        };
-    },
-
-    componentDidUpdate(nextProps) {
-        if (this.props.rule !== nextProps.rule) {
-            this.setState({rule: this.props.rule})
-        }
-    },
-
-    componentDidMount() {
+    useEffect(() => {
         AlertListActions.list().then(lists => {
-            this.setState({lists: lists});
+            setState({ ...state, lists: lists });
         });
-    },
+    }, []);
 
-    _availableRuleType() {
-        return [
-            {value: "1", label: <FormattedMessage id="wizard.matchesExactly" defaultMessage="matches exactly"/>},
-            {
-                value: "-1",
-                label: <FormattedMessage id="wizard.notMatchesExactly" defaultMessage="does not match exactly"/>
-            },
-            {
-                value: "2",
-                label: <FormattedMessage id="wizard.matchesRegularExpression"
-                                         defaultMessage="matches regular expression"/>
-            },
-            {
-                value: "-2",
-                label: <FormattedMessage id="wizard.notMatchRegularExpression"
-                                         defaultMessage="does not match regular expression"/>
-            },
-            {value: "3", label: <FormattedMessage id="wizard.greaterThan" defaultMessage="is greater than"/>},
-            {value: "-3", label: <FormattedMessage id="wizard.notGreaterThan" defaultMessage="is not greater than"/>},
-            {value: "4", label: <FormattedMessage id="wizard.smallerThan" defaultMessage="is smaller than"/>},
-            {value: "-4", label: <FormattedMessage id="wizard.notSmallerThan" defaultMessage="is not smaller than"/>},
-            {value: "5", label: <FormattedMessage id="wizard.present" defaultMessage="is present"/>},
-            {value: "-5", label: <FormattedMessage id="wizard.notPresent" defaultMessage="is not present"/>},
-            {value: "6", label: <FormattedMessage id="wizard.contains" defaultMessage="contains"/>},
-            {value: "-6", label: <FormattedMessage id="wizard.notContain" defaultMessage="does not contain"/>},
-            {value: "7", label: <FormattedMessage id="wizard.listpresent" defaultMessage="is present in list"/>},
-            {
-                value: "-7",
-                label: <FormattedMessage id="wizard.listnotpresent" defaultMessage="is not present in list"/>
-            },
-        ];
-    },
+    const _getMatchDataColor = () => {
+        return (matchData.rules[rule.id] ? '#dff0d8' : '#f2dede');
+    };
 
-    _createSelectItemsListTitle(list) {
+    const _checkForm = () => {
+        if (state.rule.field !== '' &&
+            (state.rule.type === 5 || state.rule.type === -5) ||
+            state.rule.field !== '' && state.rule.value !== '' &&
+            (state.rule.type === 1 || state.rule.type === -1 ||
+                state.rule.type === 2 || state.rule.type === -2 ||
+                state.rule.type === 3 || state.rule.type === -3 ||
+                state.rule.type === 4 || state.rule.type === -4 ||
+                state.rule.type === 6 || state.rule.type === -6 ||
+                state.rule.type === 7 || state.rule.type === -7)) {
+            setState({ ...state, isValid: true });
+        }
+        if (value === '') {
+            setState({ ...state, isValid: false });
+        }
+    };
+
+    const _updateAlertField = (field, value) => {
+        // TODO why the clone here?
+        const update = ObjectUtils.clone(state.rule);
+        update[field] = value;
+        setState({
+            ...state,
+            rule: update,
+            isModified: true
+        });
+        _checkForm();
+    };
+
+    const _onValueChanged = (field) => {
+        return e => {
+            _updateAlertField(field, e.target.value);
+        };
+    };
+
+    const _onListTypeSelect = (value) => {
+        _updateAlertField('value', value)
+    };
+
+    const _onRuleFieldSelect = (event) => {
+        const value = FormsUtils.getValueFromInput(event.target);
+        _updateAlertField('field', value);
+    };
+
+    const _onRuleTypeSelect = (value) => {
+        // TODO parseInt('') returns NaN which is a problem later on (will turn into null after a call to ObjectUtils.clone
+        _updateAlertField('type', parseInt(value));
+    };
+
+    const _createSelectItemsListTitle = (list) => {
         let items = [];
 
         if (list !== null) {
@@ -118,137 +147,100 @@ const FieldRule = createReactClass({
             }
         }
         return items;
-    },
+    };
 
-    _onListTypeSelect(value) {
-        this._updateAlertField('value', value)
-    },
+    const _update = () => {
+        onUpdate(index, state.rule, state.isValid);
+    };
 
-    _onRuleTypeSelect(value) {
-        // TODO parseInt('') returns NaN which is a problem later on (will turn into null after a call to ObjectUtils.clone
-        this._updateAlertField('type', parseInt(value));
-    },
+    const _delete = () => {
+        onDelete(index);
+    };
 
-    _onRuleFieldSelect(event) {
-        const value = FormsUtils.getValueFromInput(event.target);
-        this._updateAlertField('field', value);
-    },
+    const intl = useIntl();
+    const messages = {
+        delete: intl.formatMessage({id: "wizard.delete", defaultMessage: "Delete"}),
+        select: intl.formatMessage({id: "wizard.select", defaultMessage: "Select..."})
+    };
 
-    _updateAlertField(field, value) {
-        const update = ObjectUtils.clone(this.state.rule);
-        update[field] = value;
-        this.setState({rule: update});
-        this.setState({isModified: true});
-        this._checkForm();
-    },
+    const isMatchDataPesent = (matchData && matchData.rules.hasOwnProperty(rule.id));
+    const color = (isMatchDataPesent ? _getMatchDataColor() : '');
 
-    _onValueChanged(field) {
-        return e => {
-            this._updateAlertField(field, e.target.value);
-        };
-    },
+    const valueBox = ((state.rule.type !== 5 && state.rule.type !== -5 && state.rule.type !== 7 && state.rule.type !== -7) ?
+        <Input style={{
+                backgroundColor: color,
+                borderTopLeftRadius: '0px',
+                borderBottomLeftRadius: '0px',
+                height: '36px'
+            }}
+               id="value" name="value" type="text"
+               onChange={_onValueChanged("value")} value={state.rule.value}/>
+        : (state.rule.type === 7 || state.rule.type === -7) ?
+            <Input id="alertLists" name="alertLists">
+                <div style={{width: '150px'}}>
+                    <Select style={{backgroundColor: color, borderRadius: '0px'}}
+                            autosize={false}
+                            required
+                            clearable={false}
+                            value={state.rule.value}
+                            options={_createSelectItemsListTitle(state.lists)}
+                            matchProp="value"
+                            onChange={_onListTypeSelect}
+                            placeholder={messages.select}/>
+                </div>
+            </Input>
+            : <span style={{marginRight: 199}}/>);
 
-    _update() {
-        this.props.onUpdate(this.props.index, this.state.rule, this.state.isValid);
-    },
-    _delete() {
-        this.props.onDelete(this.props.index);
-    },
+    const deleteAction = (
+        <button id="delete-alert" type="button" className="btn btn-md btn-primary"
+                title={messages.delete} style={{marginRight: '0.5em'}}
+                onClick={_delete}>
+            <IconRemove/>
+        </button>
+    );
 
-    _isLoading() {
-        return (!this.state.rule);
-    },
+    if (state.isModified) {
+        setState({ ...state, isModified: false });
+        _update();
+    }
 
-    _checkForm() {
-        if (this.state.rule.field !== '' &&
-            (this.state.rule.type === 5 || this.state.rule.type === -5) ||
-            this.state.rule.field !== '' && this.state.rule.value !== '' &&
-            (this.state.rule.type === 1 || this.state.rule.type === -1 ||
-                this.state.rule.type === 2 || this.state.rule.type === -2 ||
-                this.state.rule.type === 3 || this.state.rule.type === -3 ||
-                this.state.rule.type === 4 || this.state.rule.type === -4 ||
-                this.state.rule.type === 6 || this.state.rule.type === -6 ||
-                this.state.rule.type === 7 || this.state.rule.type === -7)) {
-            this.setState({isValid: true});
-        }
-        if (value === '') {
-            this.setState({isValid: false});
-        }
-    },
-    _getMatchDataColor() {
-        return (this.props.matchData.rules[this.props.rule.id] ? '#dff0d8' : '#f2dede');
-    },
-
-    render() {
-        const isMatchDataPesent = (this.props.matchData && this.props.matchData.rules.hasOwnProperty(this.props.rule.id));
-        const color = (isMatchDataPesent ? this._getMatchDataColor() : '');
-
-        const valueBox = ((this.state.rule.type !== 5 && this.state.rule.type !== -5 && this.state.rule.type !== 7 && this.state.rule.type !== -7) ?
-            <Input style={{
-                    backgroundColor: color,
-                    borderTopLeftRadius: '0px',
-                    borderBottomLeftRadius: '0px',
-                    height: '36px'
-                }}
-                   id="value" name="value" type="text"
-                   onChange={this._onValueChanged("value")} value={this.state.rule.value}/>
-            : (this.state.rule.type === 7 || this.state.rule.type === -7) ?
-                <Input id="alertLists" name="alertLists">
-                    <div style={{width: '150px'}}>
-                        <Select style={{backgroundColor: color, borderRadius: '0px'}}
-                                autosize={false}
-                                required
-                                clearable={false}
-                                value={this.state.rule.value}
-                                options={this._createSelectItemsListTitle(this.state.lists)}
-                                matchProp="value"
-                                onChange={this._onListTypeSelect}
-                                placeholder={this.state.messages.select}/>
+    if (state.lists) {
+        return (
+            <div className="form-inline">
+                {deleteAction}
+                <Input id="field" name="field">
+                    <div style={{width: '200px'}}>
+                        <TypeAheadFieldInput id="field-input" type="text" required name="field" defaultValue={state.rule.field} onChange={_onRuleFieldSelect} autoFocus />
                     </div>
                 </Input>
-                : <span style={{marginRight: 199}}/>);
-
-        const deleteAction = (
-            <button id="delete-alert" type="button" className="btn btn-md btn-primary"
-                    title={this.state.messages.delete} style={{marginRight: '0.5em'}}
-                    onClick={this._delete}>
-                <IconRemove/>
-            </button>
+                <Input id="type" name="type">
+                    <div style={{width: '200px'}}>
+                        <Select style={{backgroundColor: color}}
+                                required
+                                clearable={false}
+                                value={state.rule.type.toString()}
+                                options={_availableRuleType()}
+                                matchProp="value"
+                                onChange={_onRuleTypeSelect}
+                                placeholder={messages.select}
+                        />
+                    </div>
+                </Input>
+                {valueBox}
+            </div>
         );
+    }
+    return <Spinner/>
+}
 
-        if (this.state.isModified) {
-            this.setState({isModified: false});
-            this._update();
-        }
+FieldRule.propTypes = {
+    rule: PropTypes.object.isRequired,
+    onUpdate: PropTypes.func.isRequired,
+    onDelete: PropTypes.func.isRequired,
+    // TODO isRequired?
+    matchData: PropTypes.array,
+    // TODO isRequired?
+    index: PropTypes.number
+};
 
-        if (this.state.lists) {
-            return (
-                <form className="form-inline">
-                    {deleteAction}
-                    <Input id="field" name="field">
-                        <div style={{width: '200px'}}>
-                            <TypeAheadFieldInput id="field-input" type="text" required name="field" defaultValue={this.state.rule.field} onChange={this._onRuleFieldSelect} autoFocus />
-                        </div>
-                    </Input>
-                    <Input id="type" name="type">
-                        <div style={{width: '200px'}}>
-                            <Select style={{backgroundColor: color}}
-                                    required
-                                    clearable={false}
-                                    value={this.state.rule.type.toString()}
-                                    options={this._availableRuleType()}
-                                    matchProp="value"
-                                    onChange={this._onRuleTypeSelect}
-                                    placeholder={this.state.messages.select}
-                            />
-                        </div>
-                    </Input>
-                    {valueBox}
-                </form>
-            );
-        }
-        return <Spinner/>
-    },
-});
-
-export default injectIntl(FieldRule);
+export default FieldRule;
