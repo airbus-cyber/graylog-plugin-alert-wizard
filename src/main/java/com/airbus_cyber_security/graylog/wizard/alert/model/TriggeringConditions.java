b24cd62ca42082900c7cc849b47a79f2b0c300b4
c8y3 <25362953+c8y3@users.noreply.github.com>
2024-05-15 15:27:56 +0200

    Added class TriggeringConditions

---
+++ src/main/java/com/airbus_cyber_security/graylog/wizard/alert/model/TriggeringConditions.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2018 Airbus CyberSecurity (SAS)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Server Side Public License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+
+package com.airbus_cyber_security.graylog.wizard.alert.model;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.auto.value.AutoValue;
+
+import javax.annotation.Nullable;
+import javax.validation.constraints.NotNull;
+
+@AutoValue
+@JsonAutoDetect
+public abstract class TriggeringConditions {
+
+    @JsonProperty("stream")
+    @NotNull
+    public abstract String streamIdentifier();
+
+    @JsonProperty("pipeline")
+    @Nullable
+    public abstract String pipelineIdentifier();
+
+    // TODO is this really necessary? Can't we get it from the pipeline?
+    @JsonProperty("pipeline_rule")
+    @Nullable
+    public abstract String pipelineRuleIdentifier();
+}
