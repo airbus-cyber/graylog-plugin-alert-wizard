23e34ddd71a3c08ac9c491728ccd8a0114015052
c8y3 <25362953+c8y3@users.noreply.github.com>
2024-06-12 11:17:42 +0200

    Rewrote component in functional style

--- src/web/wizard/components/rules/AlertRuleSelectionList.jsx
+++ src/web/wizard/components/rules/AlertRuleSelectionList.jsx
@@ -16,118 +16,112 @@
  */
 
 import React from 'react';
-import createReactClass from 'create-react-class';
+import { useState } from 'react';
+
 import { FormattedMessage } from 'react-intl';
 import { SearchForm } from 'components/common';
 import { Input } from 'components/bootstrap';
 import ControlledTableList from 'components/common/ControlledTableList';
 
+const AlertRuleSelectionList = ({alertRules, emptyMessage, onRuleSelectionChanged}) => {
+    const [alertTitlesFilter, setAlertTitlesFilter] = useState('');
+    // TODO simplify
+    const [state, setState] = useState({selectedAlertTitles: new Set()});
 
-const AlertRuleSelectionList = createReactClass({
+    const onSearch = (filter) => {
+        setAlertTitlesFilter(filter);
+    };
 
-    getInitialState() {
-        return {
-            alertTitlesFilter: '',
-            selectedAlertTitles: new Set()
-        };
-    },
+    const onReset = () => {
+        setAlertTitlesFilter('');
+    };
 
-    onSearch(filter) {
-        this.setState({ alertTitlesFilter: filter });
-    },
-
-    onReset() {
-        this.setState({ alertTitlesFilter: '' });
-    },
-
-    toggleSelectAll(event) {
-        let newSelection = new Set()
-        if (event.target.checked) {
-            this.props.alertRules.forEach(rule => newSelection.add(rule.title))
-        }
-
-        this.setState({ selectedAlertTitles: newSelection });
-        this.props.onRuleSelectionChanged(newSelection);
-    },
-
-    handleRuleSelect(event, title) {
-        const { selectedAlertTitles } = this.state;
+    const handleRuleSelect = (event, title) => {
+        const { selectedAlertTitles } = state;
         if (event.target.checked) {
             selectedAlertTitles.add(title);
         } else {
             selectedAlertTitles.delete(title);
         }
-        this.setState({ selectedAlertTitles: selectedAlertTitles });
-        this.props.onRuleSelectionChanged(selectedAlertTitles);
-    },
+        setState({ selectedAlertTitles: selectedAlertTitles });
+        onRuleSelectionChanged(selectedAlertTitles);
+    };
 
-    formatAlertRule(alertRule) {
-        const { selectedAlertTitles } = this.state;
+    const formatAlertRule = (alertRule) => {
         // TODO think about it, try to remove the inline "style". And add this to the coding rules
         return (
             <ControlledTableList.Item key={`alertRule_${alertRule.title}`}>
                 <Input id={`alertRule_${alertRule.title}`}
                        type="checkbox"
-                       checked={selectedAlertTitles.has(alertRule.title)}
-                       onChange={event => this.handleRuleSelect(event, alertRule.title)}
+                       checked={state.selectedAlertTitles.has(alertRule.title)}
+                       onChange={event => handleRuleSelect(event, alertRule.title)}
                        label={alertRule.title} />
                 <p className="description" style={{'margin-left': '20px'}}>{alertRule.description}</p>
             </ControlledTableList.Item>
         );
-    },
+    };
 
-    formatAlertRules() {
-        return this.props.alertRules
+    // TODO remove function: write directly formattedAlertRules = ...
+    const formatAlertRules = () => {
+        return alertRules
             .sort((rule1, rule2) => rule1.title.localeCompare(rule2.title))
-            .filter(rule => rule.title.includes(this.state.alertTitlesFilter))
-            .map(this.formatAlertRule, this);
-    },
+            .filter(rule => rule.title.includes(alertTitlesFilter))
+            .map(formatAlertRule);
+    };
 
-    render() {
-        let alerts
-        if (this.props.alertRules && this.props.alertRules.length !== 0) {
-            const { selectedAlertTitles } = this.state;
-            const selectedItemsCount = selectedAlertTitles.size
-            const label = <FormattedMessage id="wizard.selected"
-                                            defaultMessage="{ruleCount, plural, =0 {Select all} =1 {One rule selected} other {# rules selected}}"
-                                            values={{ruleCount: selectedItemsCount}} />
-            alerts = (
-                <ControlledTableList>
-                    <ControlledTableList.Header>
-                        <Input id="select-all-checkbox"
-                               type="checkbox"
-                               label={label}
-                               checked={selectedItemsCount === this.props.alertRules.length}
-                               onChange={this.toggleSelectAll}
-                               wrapperClassName="form-group-inline"
-                        />
-
-                    </ControlledTableList.Header>
-                    {this.formatAlertRules()}
-                </ControlledTableList>
-            )
-        } else {
-            alerts = (
-                <span className="help-block help-standalone">
-                    {this.props.emptyMessage}
-                </span>
-            )
+    const toggleSelectAll = (event) => {
+        let newSelection = new Set()
+        if (event.target.checked) {
+            alertRules.forEach(rule => newSelection.add(rule.title))
         }
 
-        return (
-            <>
-                <SearchForm onSearch={this.onSearch}
-                            onReset={this.onReset}
-                            searchButtonLabel="Filter"
-                            placeholder="Filter alert rules by title..."
-                            queryWidth={400}
-                            resetButtonLabel="Reset"
-                            searchBsStyle="info"
-                            topMargin={0} />
-                {alerts}
-            </>
+        setState({ selectedAlertTitles: newSelection });
+        onRuleSelectionChanged(newSelection);
+    };
+
+    // TODO create a method instead
+    let alerts
+    if (alertRules && alertRules.length !== 0) {
+        const selectedItemsCount = state.selectedAlertTitles.size
+        const label = <FormattedMessage id="wizard.selected"
+                                        defaultMessage="{ruleCount, plural, =0 {Select all} =1 {One rule selected} other {# rules selected}}"
+                                        values={{ruleCount: selectedItemsCount}} />
+        alerts = (
+            <ControlledTableList>
+                <ControlledTableList.Header>
+                    <Input id="select-all-checkbox"
+                           type="checkbox"
+                           label={label}
+                           checked={selectedItemsCount === alertRules.length}
+                           onChange={toggleSelectAll}
+                           wrapperClassName="form-group-inline"
+                    />
+
+                </ControlledTableList.Header>
+                {formatAlertRules()}
+            </ControlledTableList>
+        )
+    } else {
+        alerts = (
+            <span className="help-block help-standalone">
+                {emptyMessage}
+            </span>
         )
     }
-})
+
+    return (
+        <>
+            <SearchForm onSearch={onSearch}
+                        onReset={onReset}
+                        searchButtonLabel="Filter"
+                        placeholder="Filter alert rules by title..."
+                        queryWidth={400}
+                        resetButtonLabel="Reset"
+                        searchBsStyle="info"
+                        topMargin={0} />
+            {alerts}
+        </>
+    )
+}
 
 export default AlertRuleSelectionList;
