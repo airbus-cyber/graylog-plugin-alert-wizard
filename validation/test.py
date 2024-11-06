ac220d286d08ba897f3bdb37e090e253084f2d87
c8y3 <25362953+c8y3@users.noreply.github.com>
2024-11-06 14:45:08 +0100

    Started a test suite which does not need for the graylog server to be restarted between each tests

--- validation/test.py
+++ validation/test.py
@@ -29,23 +29,11 @@ class Test(TestCase):
     def tearDown(self) -> None:
         self._graylog.stop()
 
-    def test_get_alerts_should_be_found(self):
-        status_code = self._graylog.get_alert_rules()
-        self.assertEqual(200, status_code)
-
     def test_put_config_with_time_default_value_should_modify_time_default_value(self):
         default_time = 1441
         self._graylog.update_alert_wizard_plugin_configuration(default_time=default_time)
         configuration = self._graylog.get_alert_wizard_plugin_configuration()
         self.assertEqual(default_time, configuration['default_values']['time'])
-
-    def test_create_alert_rule_should_not_fail(self):
-        alert_rule = self._graylog.create_alert_rule_count('alert_rule_title', _PERIOD)
-        self.assertEqual('alert_rule_title', alert_rule['title'])
-
-    def test_create_alert_rule_statistics_should_not_fail(self):
-        alert_rule = self._graylog.create_alert_rule_statistics('statistics', _PERIOD)
-        self.assertEqual('statistics', alert_rule['title'])
 
     def test_set_logging_alert_configuration_should_not_fail(self):
         status_code = self._graylog.update_logging_alert_plugin_configuration()
@@ -58,18 +46,6 @@ class Test(TestCase):
         rule = self._graylog.create_alert_rule_count(title, _PERIOD)
         notification = self._graylog.get_notification(rule['notification'])
         self.assertEqual(1441, notification['config']['aggregation_time'])
-
-    def test_get_alert_rule_should_return_correct_additional_threshold_type__issue34(self):
-        title = 'rule_title'
-        self._graylog.create_alert_rule_and(title, _PERIOD)
-        retrieved_alert_rule = self._graylog.get_alert_rule(title)
-        self.assertEqual('<', retrieved_alert_rule['condition_parameters']['additional_threshold_type'])
-
-    def test_get_alert_rule_should_return_correct_additional_threshold__issue69(self):
-        title = 'rule_title'
-        self._graylog.create_alert_rule_and(title, _PERIOD, additional_threshold=1)
-        retrieved_alert_rule = self._graylog.get_alert_rule(title)
-        self.assertEqual(1, retrieved_alert_rule['condition_parameters']['additional_threshold'])
 
     def test_create_list_should_create_data_adapter(self):
         self._graylog.create_list('test', ['a'])
---
+++ validation/tests_fast.py
@@ -0,0 +1,58 @@
+# These tests do not require for the graylog server to be shutdown and started between each of them
+# Ideally all tests should be run in this suite, so that the start/shutdown of Graylog is done outside the tests
+#
+# to create and populate the test venv:
+# * python3 -m venv venv
+# * source venv/bin/activate
+# * pip install -r requirements.txt
+# to execute these tests:
+# * activate venv
+#   source ./venv/bin/activate
+# * execute tests
+#   python -m unittest --verbose
+# To execute only one test, suffix with the fully qualified test name. Example:
+#   python -m unittest test.Test.test_create_alert_rule_with_list_should_generate_event_when_message_field_is_in_list
+
+from unittest import TestCase
+import time
+from graylog import Graylog
+from uuid import uuid4
+
+_PERIOD = 1
+
+class TestsFast(TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls._graylog = Graylog()
+        cls._graylog.start()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls._graylog.stop()
+
+    def test_get_alerts_should_be_found(self):
+        status_code = self._graylog.get_alert_rules()
+        self.assertEqual(200, status_code)
+
+    def test_create_alert_rule_should_not_fail(self):
+        rule_title = f'alert_rule_title_{uuid4()}'
+        alert_rule = self._graylog.create_alert_rule_count(rule_title, _PERIOD)
+        self.assertEqual(rule_title, alert_rule['title'])
+
+    def test_create_alert_rule_statistics_should_not_fail(self):
+        rule_title = f'statistics_{uuid4()}'
+        alert_rule = self._graylog.create_alert_rule_statistics(rule_title, _PERIOD)
+        self.assertEqual(rule_title, alert_rule['title'])
+
+    def test_get_alert_rule_should_return_correct_additional_threshold_type__issue34(self):
+        title = f'rule_title_{uuid4()}'
+        self._graylog.create_alert_rule_and(title, _PERIOD)
+        retrieved_alert_rule = self._graylog.get_alert_rule(title)
+        self.assertEqual('<', retrieved_alert_rule['condition_parameters']['additional_threshold_type'])
+
+    def test_get_alert_rule_should_return_correct_additional_threshold__issue69(self):
+        title = f'rule_title_{uuid4()}'
+        self._graylog.create_alert_rule_and(title, _PERIOD, additional_threshold=1)
+        retrieved_alert_rule = self._graylog.get_alert_rule(title)
+        self.assertEqual(1, retrieved_alert_rule['condition_parameters']['additional_threshold'])
