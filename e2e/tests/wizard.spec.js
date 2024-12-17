a91d97b7806c8114de548b179e719a3deb5b9a02
VincentD06 <vincent.dautreme@airbus.com>
2024-12-17 16:05:20 +0100

    Playwright : refactor + add some wait

--- e2e/tests/wizard.spec.js
+++ e2e/tests/wizard.spec.js
@@ -7,8 +7,8 @@ test('statistics rule should retain field', async ({ page }) => {
 
   await _login_steps(page);
 
+  // Fill Title
   const title = `AAA - ${crypto.randomUUID()}`;
-
   await page.getByRole('link', { name: 'Create' }).click();
   await page.getByRole('button', { name: 'Statistics' }).click();
   await page.locator('#title').fill(title);
@@ -20,8 +20,11 @@ test('statistics rule should retain field', async ({ page }) => {
   await page.locator('#react-select-9-input').fill('source');
   await page.getByRole('option', { name: 'source – string' }).click();
   await page.getByRole('button', { name: 'Save' }).click();
+  //await page.waitForTimeout(500);
 
-  await page.getByPlaceholder('Filter alert rules').fill(title);
+  // Go on search page
+  await _open_alert_page_and_filter(page, title);
+
   await expect(page.getByRole('link', { name: 'Edit' })).toHaveCount(1);
   await page.getByRole('link', { name: 'Edit' }).click();
   await expect(page.getByText('source – string')).toBeVisible();
@@ -40,6 +43,7 @@ test('go_on_search_page_when_click_on_search_button', async ({ page }) => {
 
   // Add Field Condition
   await page.getByRole('button', { name: 'add_circle' }).click();
+  await page.waitForTimeout(200);
   await page.locator('#field-input').fill('message');
   await page.waitForTimeout(200);
   await page.getByText('arrow_drop_down').nth(2).click();
@@ -49,13 +53,13 @@ test('go_on_search_page_when_click_on_search_button', async ({ page }) => {
   // Fill Search Query
   const searchQuery = 'a?c';
   await page.locator('#search_query').fill(searchQuery);
+  await page.waitForTimeout(200);
 
   // Save
   await page.getByRole('button', { name: 'Save' }).click();
 
   // Go on search page
-  await page.getByPlaceholder('Filter alert rules').fill(title);
-  await page.waitForTimeout(2000);
+  await _open_alert_page_and_filter(page, title);
   await page.getByRole('link', { name: 'play_arrow' }).click();
 
   await expect(page.getByText(title)).toBeVisible();
@@ -66,4 +70,13 @@ async function _login_steps(page) {
   await page.getByLabel('Username').fill('admin');
   await page.getByLabel('Password').fill('admin');
   await page.getByLabel('Sign in').click();
+}
+
+async function _open_alert_page_and_filter(page, filter) {
+  await page.goto('/wizard/AlertRules');
+  // Wait for rules are loaded
+  await page.waitForTimeout(1000);
+  await page.getByPlaceholder('Filter alert rules').fill(filter);
+  // Wait for filter is applied
+  await page.waitForTimeout(500);
 }
