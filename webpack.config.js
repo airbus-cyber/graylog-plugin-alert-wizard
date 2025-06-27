e218f6be1dae66a08274c5806ee49524436d2f12
VincentD06 <vincent.dautreme@airbus.com>
2025-06-27 11:44:00 +0200

    Bump Graylog version to 6.1.4

--- webpack.config.js
+++ webpack.config.js
@@ -5,4 +5,9 @@ const path = require('path');
 // Remember to use the same name here and in `getUniqueId()` in the java MetaData class
 module.exports = new PluginWebpackConfig(__dirname, 'com.airbus_cyber_security.graylog.AlertWizardPlugin', loadBuildConfig(path.resolve(__dirname, './build.config')), {
   // Here goes your additional webpack configuration.
+
+    performance: {
+        maxEntrypointSize: 3074000,
+        maxAssetSize: 2048000
+    }
 });
