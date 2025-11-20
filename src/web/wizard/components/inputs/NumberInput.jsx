b3ee98bc7c8c1ff47b80f13600bf5093450e5735
VincentD06 <vincent.dautreme@airbus.com>
2025-11-20 17:28:03 +0100

    Rewrite NumberInput in functionnal mode

--- src/web/wizard/components/inputs/NumberInput.jsx
+++ src/web/wizard/components/inputs/NumberInput.jsx
@@ -15,84 +15,58 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 
-import PropTypes from 'prop-types';
-import React from 'react';
-import createReactClass from 'create-react-class';
-import { injectIntl, FormattedMessage } from 'react-intl';
+import React, {useRef} from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
 import { Input, Row, Col } from 'components/bootstrap';
 import { Select } from 'components/common';
 
-// TODO rewrite into functional form
-const NumberInput = createReactClass({
-    displayName: 'NumberInput',
+const NumberInput = ({onUpdate, threshold = '', threshold_type = ''}) => {
+    const thresholdRef = useRef();
+    const thresholdTypeRef = useRef();
 
-    propTypes: {
-        onUpdate: PropTypes.func,
-    },   
-    getDefaultProps() {
-        return {
-            threshold_type: '',
-            threshold: '',
-        };
-    },
-    getInitialState() {
-        return {
-            threshold_type: this.props.threshold_type,
-            threshold: this.props.threshold,
-        };
-    },
-    // TODO should factor constants with ManageSettings
-    _availableThresholdTypes() {
-        return [
-            {value: '>', label: <FormattedMessage id="wizard.more" defaultMessage="more than" />},
-            {value: '<', label: <FormattedMessage id="wizard.less" defaultMessage="less than" />},
-        ];
-    },
-    _onThresholdTypeSelect(id) {
-        this.setState({threshold_type: id});
-        this.props.onUpdate('threshold_type', id)
-    },
-    _onThresholdChanged() {
-        return e => {
-            this.setState({threshold: e.target.value});
-            this.props.onUpdate('threshold', e.target.value);
-        };
-    },
-    
-    render() {
-        const { intl } = this.props;
+    const intl = useIntl();
 
-        return (
-            <Row>
-                <Col md={2} style={{ marginTop: 5, marginBottom: 0 }}>
-                    <label className="pull-right"><FormattedMessage id="wizard.NumberInput" defaultMessage="Count Condition" /></label>
-                </Col>
-                <Col md={10}>
-                    <label><FormattedMessage id="wizard.thereMustBe" defaultMessage="There must be " /></label>
-                    <Input ref="threshold_type" id="threshold_type" name="threshold_type" required>
-                        <div style={{width:'150px'}}>
-                        <Select
-                            required
-                            clearable={false}
-                            value={this.state.threshold_type}
-                            options={this._availableThresholdTypes()}
-                            matchProp="value"
-                            onChange={this._onThresholdTypeSelect}
-                            placeholder={intl.formatMessage({ id: "wizard.select", defaultMessage: "Select..." })}
-                        />
-                        </div>
-                    </Input>
-                    <Input ref="threshold" id="threshold" name="threshold" type="number" min="0" onChange={this._onThresholdChanged()}
-                           value={this.state.threshold}
-                           style={{borderTopLeftRadius: '0px', borderBottomLeftRadius: '0px', height:'36px', width:'100px'}} />
-                    <label>&nbsp; </label>
-                    <label><FormattedMessage id="wizard.messages" defaultMessage="messages" /></label>
-                    
-                </Col>
-            </Row>
-        );
-        
-    },
-});
+    const _availableThresholdTypes = [
+        {value: '>', label: <FormattedMessage id="wizard.more" defaultMessage="more than" />},
+        {value: '<', label: <FormattedMessage id="wizard.less" defaultMessage="less than" />}
+    ];
 
-export default injectIntl(NumberInput);
+    const _onThresholdTypeSelect = (id) => {
+        onUpdate('threshold_type', id);
+    };
+
+    const _onThresholdChanged = e => {
+        onUpdate('threshold', e.target.value);
+    };
+
+    return (
+        <Row>
+            <Col md={2} style={{ marginTop: 5, marginBottom: 0 }}>
+                <label className="pull-right"><FormattedMessage id="wizard.NumberInput" defaultMessage="Count Condition" /></label>
+            </Col>
+            <Col md={10}>
+                <label><FormattedMessage id="wizard.thereMustBe" defaultMessage="There must be " /></label>
+                <Input ref={thresholdTypeRef} id="threshold_type" name="threshold_type" required>
+                    <div style={{width:'150px'}}>
+                    <Select
+                        required
+                        clearable={false}
+                        value={threshold_type}
+                        options={_availableThresholdTypes}
+                        matchProp="value"
+                        onChange={_onThresholdTypeSelect}
+                        placeholder={intl.formatMessage({ id: "wizard.select", defaultMessage: "Select..." })}
+                    />
+                    </div>
+                </Input>
+                <Input ref={thresholdRef} id="threshold" name="threshold" type="number" min="0" onChange={_onThresholdChanged}
+                       value={threshold}
+                       style={{borderTopLeftRadius: '0px', borderBottomLeftRadius: '0px', height:'36px', width:'100px'}} />
+                <label>&nbsp; </label>
+                <label><FormattedMessage id="wizard.messages" defaultMessage="messages" /></label>
+            </Col>
+        </Row>
+    );
+};
+
+export default NumberInput;
