603fe5c101addca5f7ea66a1c8db2ce3265ea1a0
VincentD06 <vincent.dautreme@airbus.com>
2026-01-06 10:54:40 +0100

    FIX Wizard create/update STATISTIC rule

--- src/web/wizard/components/conditions/StatisticsCondition.jsx
+++ src/web/wizard/components/conditions/StatisticsCondition.jsx
@@ -15,78 +15,63 @@
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 
-
-import PropTypes from 'prop-types';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ObjectUtils from 'util/ObjectUtils';
 import FieldsInput from 'wizard/components/inputs/FieldsInput';
 import TimeRangeInput from 'wizard/components/inputs/TimeRangeInput';
 import StatisticalInput from 'wizard/components/inputs/StatisticalInput';
 import Description from 'wizard/components/inputs/Description';
-import SearchQueryInput from '../inputs/SearchQueryInput';
+import SearchQueryInput from 'wizard/components/inputs/SearchQueryInput';
+import TimeHook from './TimeHook';
 
+const StatisticsCondition = ({alert, onUpdate}) => {
 
-// TODO convert into a functional component
-class StatisticsCondition extends React.Component {
+    const [state, setState] = useState({ alert: ObjectUtils.clone(alert), time: 0, time_type: 0 });
 
-    static propTypes = {
-        onUpdate: PropTypes.func,
-    }
+    useEffect(() => {
+        const { time, time_type } = TimeHook.computeTime(alert);
+        setState({ alert, time, time_type });
+    }, []);
 
-    state = {
-        alert: ObjectUtils.clone(this.props.alert),
-    }
+    const _handleChangeCondition = (field, value) => {
+        const update = ObjectUtils.clone(state.alert);
 
-    _handleChangeCondition(field, value) {
-        let update = ObjectUtils.clone(this.state.alert);
         if (field === "threshold" || field === "additional_threshold" || field === "time") {
             update.condition_parameters[field] = parseInt(value);
         } else {
             update.condition_parameters[field] = value;
         }
-        this.setState({alert: update});
-        this.props.onUpdate('condition_parameters', update.condition_parameters);
-    }
 
-    _handleChangeStream(field, value) {
-        let update = ObjectUtils.clone(this.state.alert);
+        const { time, time_type } = TimeHook.computeTime(update);
+        setState({ alert: update, time, time_type });
+        onUpdate('condition_parameters', update.condition_parameters);
+    };
+
+    const _handleChangeStream = (field, value) => {
+        const update = ObjectUtils.clone(state.alert);
         update.stream[field] = value;
-        this.setState({alert: update});
-        this.props.onUpdate('stream', update.stream);
-    }
-    
-    render() {
-        let time;
-        let time_type;
-        if (this.props.alert.condition_parameters.time >= 1440) {
-            time = this.props.alert.condition_parameters.time / 1440;
-            time_type = 1440;
-        } else if (this.props.alert.condition_parameters.time >= 60) {
-            time = this.props.alert.condition_parameters.time / 60;
-            time_type = 60;
-        } else {
-            time = this.props.alert.condition_parameters.time;
-            time_type = 1;
-        }
-        
-        return (
-            <>
-                <SearchQueryInput onUpdate={this._handleChangeCondition} search_query={this.props.alert.condition_parameters.search_query} />
-                <br/>
-                <FieldsInput stream={this.props.alert.stream} onSaveStream={this._handleChangeStream} message={this.props.message}
-                            matchData={this.props.matchData} />
-                <br/>
-                <TimeRangeInput onUpdate={this._handleChangeCondition} time={time.toString()} time_type={time_type.toString()} />
-                <br/>
-                <StatisticalInput onUpdate={this._handleChangeCondition} type={this.props.alert.condition_parameters.type}
-                                  field={this.props.alert.condition_parameters.field} threshold={this.props.alert.condition_parameters.threshold}
-                                  thresholdType={this.props.alert.condition_parameters.threshold_type}/>
-                <br/>
-                <Description onUpdate={this.props.onUpdate} description={this.props.alert.description}/>
-                <br/>
-            </>
-        );
-    }
+
+        const { time, time_type } = TimeHook.computeTime(update);
+        setState({ alert: update, time, time_type });
+        onUpdate('stream', update.stream);
+    };
+
+    return (
+        <>
+            <SearchQueryInput onUpdate={_handleChangeCondition} search_query={state.alert.condition_parameters.search_query} />
+            <br/>
+            <FieldsInput stream={state.alert.stream} onSaveStream={_handleChangeStream} />
+            <br/>
+            <TimeRangeInput onUpdate={_handleChangeCondition} time={state.time.toString()} time_type={state.time_type.toString()} />
+            <br/>
+            <StatisticalInput onUpdate={_handleChangeCondition} type={state.alert.condition_parameters.type}
+                              field={state.alert.condition_parameters.field} threshold={state.alert.condition_parameters.threshold}
+                              thresholdType={state.alert.condition_parameters.threshold_type}/>
+            <br/>
+            <Description onUpdate={onUpdate} description={state.alert.description}/>
+            <br/>
+        </>
+    );
 }
 
 export default StatisticsCondition;
